c5e9f72885b323b557dbc35cfafd06bb570308c2
Mutuking <kawaguchi.2525.haruki@gmail.com>
2025-01-14 22:46:25 +0900

    MT4_01_02完了

--- application/scene/GameScene.cpp
+++ application/scene/GameScene.cpp
@@ -17,10 +17,21 @@ void GameScene::Finalize()
 
 void GameScene::Update()
 {
-	Vector3 axis = Normalize(Vector3{ 1.0f, 1.0f, 1.0f });
-	float angle = 0.44f;
-	Matrix4x4 rotateMatrix = MakeRotateAxisAngle(axis, angle);
-	PrintOnImGui(rotateMatrix);
+    Vector3 from0 = Normalize(Vector3{ 1.0f, 0.7f, 0.5f });
+    Vector3 to0 = from0 * -1.0f;
+
+    Vector3 from1 = Normalize(Vector3{ -0.6f, 0.9f, 0.2f });
+    Vector3 to1 = Normalize(Vector3{ 0.4f, 0.7f, -0.5f });
+
+    Matrix4x4 rotateMatrix0 = DirectionToDirection(
+        Normalize(Vector3{ 1.0f, 0.0f, 0.0f }), Normalize(Vector3{ -1.0f, 0.0f, 0.0f })
+    );
+    Matrix4x4 rotateMatrix1 = DirectionToDirection(from0, to0);
+    Matrix4x4 rotateMatrix2 = DirectionToDirection(from1, to1);
+
+    PrintOnImGui(rotateMatrix0);
+    PrintOnImGui(rotateMatrix1);
+    PrintOnImGui(rotateMatrix2);
 }
 
 void GameScene::Draw()
