9a4bbbcd78dcc2c0ba7bbb6e486935fc46108986
Mutuking <kawaguchi.2525.haruki@gmail.com>
2025-02-15 18:17:02 +0900

    MT4_01_05

--- application/scene/GameScene.cpp
+++ application/scene/GameScene.cpp
@@ -18,7 +18,7 @@ void GameScene::Finalize()
 void GameScene::Update()
 {
     Quaternion rotation0 = MakeRotateAxisAngleQuaternion({ 0.71f, 0.71f, 0.0f }, 0.3f);
-    Quaternion rotation1 = MakeRotateAxisAngleQuaternion({ 0.71f, 0.0f, 0.71f }, 3.141592f);
+    Quaternion rotation1 = { -rotation0.x, -rotation0.y, -rotation0.z , -rotation0.w };
 
     Quaternion interpolate0 = Slerp(rotation0, rotation1, 0.0f);
     Quaternion interpolate1 = Slerp(rotation0, rotation1, 0.3f);
